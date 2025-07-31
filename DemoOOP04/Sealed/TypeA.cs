2b1e996a9b6a99dcde6b5d9cc5842b61631d1635
steve.addams <steve.addams@BK-STEVEADDAMS>
2025-07-31 22:47:31 +0300

    V09

---
+++ DemoOOP04/Sealed/TypeA.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoOOP04.Sealed
+{
+    internal class TypeA
+    {
+        public virtual int A { get; set; }
+       
+        public virtual void MyFun()
+        {
+            Console.WriteLine("TypeA.MyFun");
+        }
+    }
+}
